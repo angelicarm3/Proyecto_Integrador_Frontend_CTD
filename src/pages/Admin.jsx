83bedc687737f50ca011929076462157cc3753ac
Laurajcb <callejaslaura96@gmail.com>
2024-10-24 19:05:40 -0500

    feat: 🎸 added temporary styles

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProductList from '../Common/Components/ProductList/ProductList'
+import { FaArrowCircleLeft } from "react-icons/fa";
 
 const Admin = () => {
   const handleAddProduct = () => {
@@ -7,23 +8,24 @@ const Admin = () => {
     console.log(`Agregar producto ${product.id}`)
   }
   return (
-    <div className='p-4'>
+    <div className='p-4 flex-column'>
       <h1 className='text-2xl font-bold'>Administración de Productos</h1>
-      <section>
-        <div className="p-4">
+      <section className='flex flex-row items-center'>
+      <FaArrowCircleLeft />
+        <div className='p-4 flex flex-row gap-2 items-center'>
           <input
-            type="text"
-            placeholder="Buscar productos..."
-            className="border border-gray-300 px-4 py-2 rounded w-full mb-2"
+            type='text'
+            placeholder='Buscar productos...'
+            className='border border-gray-300 px-4 py-2 rounded w-full  h-8'
           />
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-200 w-full"
+            className='bg-blue-500 text-white  h-8 px-4 py-2 rounded hover:bg-blue-600 transition duration-200 w-full'
           >
             Buscar
           </button>
         </div>
         <button
-          className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-200'
+          className='bg-blue-500 text-white px-4 py-2  h-8 rounded hover:bg-blue-600 transition duration-200'
           onClick={handleAddProduct}
         >
           Agregar Producto
