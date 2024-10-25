50c68528c8dfbe3219c73ee4230c9d714a99b120
Angelica Rodriguez <angie00069@gmail.com>
2024-10-25 08:51:31 -0500

    refactor: 💡 Atomic design files

---
+++ src/components/Organisms/Header/Header.jsx
@@ -0,0 +1,44 @@
+import isoGold from '../../../assets/brand/isoGold.svg'
+import Navbar from '../../Molecules/Navbar/Navbar'
+
+function Header () {
+  return (
+    <header style={styles.header}>
+      <img src={isoGold} alt='logo' style={styles.logo} />
+      <Navbar />
+      <div style={styles.authButtons}>
+        <button style={styles.button}>Login</button>
+        <button style={styles.button}>Signup</button>
+      </div>
+    </header>
+  )
+}
+
+const styles = {
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '10px 20px',
+    backgroundColor: 'rgb(15 23 42)'
+  },
+  logo: {
+    height: '50px',
+    marginRight: '20px'
+  },
+  authButtons: {
+    display: 'flex',
+    gap: '10px'
+  },
+  button: {
+    padding: '10px 20px',
+    backgroundColor: '#F3C02F',
+    color: 'rgb(15 23 42)',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    fontSize: '16px'
+  }
+}
+
+export default Header
