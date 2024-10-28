ed47690ce7b6f3745bd774fe94b5fbd62ec60aae
Angelica Rodriguez <angie00069@gmail.com>
2024-10-28 18:14:19 -0500

    style: 💄 Fixed header styles

---
+++ src/assets/page/assets/pageImg.jsx
@@ -0,0 +1,11 @@
+import premiumCar from './page/premiumCar.png'
+import sportsCar from './page/sportsCar.png'
+import SUVCar from './page/SUVCar.png'
+import allCars from './page/allCars.png'
+
+export {
+  premiumCar,
+  sportsCar,
+  SUVCar,
+  allCars
+}
--- src/components/Organisms/Header/Header.jsx
+++ src/components/Organisms/Header/Header.jsx
@@ -1,19 +1,32 @@
-import isoGold from '../../../assets/brand/isoGold.svg'
+import { useState } from 'react'
+
+import { Link } from 'react-router-dom'
+import { AiOutlineMenu } from 'react-icons/ai'
+
+import './header.css'
+import isoTipoGold from '../../../assets/brand/isoTipoGold.svg'
+import sloganGold from '../../../assets/brand/sloganGold.png'
+import logoGold from '../../../assets/brand/logoGold.png'
 import LogInBtn from '../../Atoms/LoginBtn/LoginBtn'
 import SignUpBtn from '../../Atoms/SignUpBtn/SignUpBtn'
 import Navbar from '../../Molecules/Navbar/Navbar'
-import { Link } from 'react-router-dom'
-import './header.css'
-
 
 function Header () {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => setIsOpen(!isOpen)
+
   return (
-    <header className='headerStyles'>
-      <Link to='/'>
-        <img src={isoGold} alt='logo' class="h-12 mr-5"/>
+    <header className='headerStyles z-40'>
+      <AiOutlineMenu size={30} className='text-yellow1 cursor-pointer md:hidden hover:opacity-75' />
+      <Link to='/' className='w-full md:w-fit flex justify-center items-center self-center md:self-start'>
+        <img src={isoTipoGold} alt='logo' className='min-w-[70-px] h-12 mr-5' />
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <img src={logoGold} alt='logo' className='h-5 object-cover' />
+          <img src={sloganGold} alt='logo' className='h-3 object-cover' />
+        </div>
       </Link>
-      <Navbar />
-      <div class="flex gap-2.5 w-fit">
+      {/* <Navbar /> */}
+      <div className='hidden md:flex gap-2.5 w-fit'>
         <LogInBtn />
         <SignUpBtn />
       </div>
