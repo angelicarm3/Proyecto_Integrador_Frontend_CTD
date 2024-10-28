be41492ecf55bd4ea1ece16dd42741524c4da4d5
Farydinho <farydalejandro1997@hotmail.com>
2024-10-28 17:30:41 -0500

    feat: 🎸 se completa el 100% de los requerimentos en jira
    menu hamburguesa al cambio de tamaño de pantalla, cambio de color y
    otras cositas

--- src/components/Organisms/Header/Header.jsx
+++ src/components/Organisms/Header/Header.jsx
@@ -3,20 +3,27 @@ import LogInBtn from '../../Atoms/LoginBtn/LoginBtn'
 import SignUpBtn from '../../Atoms/SignUpBtn/SignUpBtn'
 import Navbar from '../../Molecules/Navbar/Navbar'
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
 import './header.css'
 
 
 function Header () {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <header className='headerStyles'>
       <Link to='/'>
         <img src={isoGold} alt='logo' class="h-12 mr-5"/>
       </Link>
-      <Navbar />
-      <div class="flex gap-2.5 w-fit">
+      {/*<Navbar />*/}
+      <div class="flex gap-2.5 w-fit hidden sm:flex space-x-4">
         <LogInBtn />
         <SignUpBtn />
       </div>
+      <button onClick={toggleMenu} className="text-white sm:hidden focus:outline-none text-[rgb(212 175 55)]-50">
+          ☰
+      </button>
     </header>
   )
 }
