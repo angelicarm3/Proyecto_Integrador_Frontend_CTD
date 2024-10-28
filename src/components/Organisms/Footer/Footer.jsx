5601340bbd710b82f59bb2ec335c408e2b30c00a
vbqcid <vbqcid@gmail.com>
2024-10-27 21:01:18 -0400

    style: 💄 solo estilos

--- src/components/Organisms/Footer/Footer.jsx
+++ src/components/Organisms/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './footer.css';
-import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import isoWhite from '../../../assets/brand/isoWhite.svg'
 
 const Footer = () => {
