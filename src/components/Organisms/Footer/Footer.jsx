bc0dbc660a0a93ac6c7a645cb003ed511970fcff
Angelica Rodriguez <angie00069@gmail.com>
2024-10-28 18:49:40 -0500

    fix: 🐛 Solve merge conflicts

--- src/components/Organisms/Footer/Footer.jsx
+++ src/components/Organisms/Footer/Footer.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './footer.css';
-import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
-import isoWhite from '../../../assets/brand/isoWhite.png';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './footer.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import isoWhite from '../../../assets/brand/isoWhite.png'
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-container">
-        
-        <div className="footer-logo-container">
-          <Link to="/home">
-            <img src={isoWhite} alt="Logo de la marca" className="footer-logo" />
+    <footer className='footer'>
+      <div className='footer-container'>
+
+        <div className='footer-logo-container'>
+          <Link to='/'>
+            <img src={isoWhite} alt='Logo de la marca' className='footer-logo' />
           </Link>
         </div>
 
-        <div className="footer-copyright">
-        <p>&copy; {new Date().getFullYear()} Royal Ride. Todos los derechos reservados.</p>
-      </div>
-    
-        <div className="footer-social">
-          <Link to="https://facebook.com/" aria-label="Facebook" className="social-icon">
+        <div className='footer-copyright'>
+          <p>&copy; {new Date().getFullYear()} Royal Ride. Todos los derechos reservados.</p>
+        </div>
+
+        <div className='footer-social'>
+          <Link to='https://facebook.com/' aria-label='Facebook' className='social-icon'>
             <FaFacebook />
           </Link>
           <Link to='https://instagram.com/' aria-label='Instagram' className='social-icon'>
@@ -30,12 +30,9 @@ const Footer = () => {
             <FaLinkedin />
           </Link>
         </div>
-
-  
-
       </div>
     </footer>
   )
 }
 
-export default Footer;
+export default Footer
