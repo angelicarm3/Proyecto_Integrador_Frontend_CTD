bd2800e7bc7970d468ee7191cc70ce1cf3925979
Angelica Rodriguez <angie00069@gmail.com>
2024-11-09 20:39:21 -0500

    fix: 🐛 Resolving merge issues

--- src/components/Pages/AdminUsers/AdminUsers.jsx
+++ src/components/Pages/AdminUsers/AdminUsers.jsx
@@ -1,30 +1,30 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAllUsersAdminThunk } from '../.././../context/slices/adminUserSlice';
-import AdminUsersList from '../../Organisms/AdminUsersList/AdminUsersList';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAllUsersAdminThunk } from '../.././../context/slices/adminUserSlice'
+import AdminUsersList from '../../Organisms/AdminUsersList/AdminUsersList'
 
 const AdminUsers = () => {
-  const dispatch = useDispatch();
-  const { loading, error, success } = useSelector((state) => state.adminUsers);
+  const dispatch = useDispatch()
+  const { loading, error, success } = useSelector((state) => state.adminUsers)
 
   // Obtener usuarios al montar el componente
   useEffect(() => {
-    dispatch(fetchAllUsersAdminThunk());
-  }, [dispatch]);
+    dispatch(fetchAllUsersAdminThunk())
+  }, [dispatch])
 
   return (
-    <div className="admin-users-container p-4">
-      <h1 className="text-3xl font-semibold mb-6">Admin Users</h1>
+    <div className='admin-users-container p-4'>
+      <h1 className='text-3xl font-semibold mb-6'>Admin Users</h1>
 
       {/* Mensajes de estado */}
-      {loading && <p className="text-gray-500">Cargando Usuarios</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      {success && <p className="text-green-500">Éxito!</p>}
+      {loading && <p className='text-gray-500'>Cargando Usuarios</p>}
+      {error && <p className='text-red-500'>{error}</p>}
+      {success && <p className='text-green-500'>Éxito!</p>}
 
       {/* Lista de usuarios */}
       <AdminUsersList />
     </div>
-  );
-};
+  )
+}
 
-export default AdminUsers;
+export default AdminUsers
