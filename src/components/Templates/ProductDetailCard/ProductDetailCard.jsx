a650a73387658c7a8c506e5414b06b335eff325e
Angelica Rodriguez <angie00069@gmail.com>
2024-11-27 19:20:16 -0500

    fix: 🐛 added hascommented conditional

--- src/components/Templates/ProductDetailCard/ProductDetailCard.jsx
+++ src/components/Templates/ProductDetailCard/ProductDetailCard.jsx
@@ -37,7 +37,7 @@ const ProductDetailCard = ({ onSuccess }) => {
       (review) => review.nombreUsuario === loggedUser.nombre
     )
 
-    setCanComment(hasBooked)
+    setCanComment(hasBooked && !hasComented)
   }, [selectedProduct, loggedUser])
 
   const handleShareClick = () => {
